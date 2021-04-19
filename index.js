bc84d1ddd76025070d12d7717a1fc83b8a392fc8
shmuelsimpser <ssimpser@gmail.com>
2021-04-19 00:04:28 -0400

    coompleted assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <title>victory</title>
+  </head>
+  <script type="Sammy is the champion">
+  </script>
+  <body>
+    <main id="victory"></main>
+  </body>
+</html>
